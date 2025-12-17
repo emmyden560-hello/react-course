2a2811540ff0da28bee8ac602f2fcf2af7742dfd
emmyd <emmanueldenis560@gmail.com>
2025-12-17 10:56:31 +0100

    made a few changes still can't make the checkout page to work

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/homepage'
+import { CheckoutPage } from './pages/checkoutpage';
 import './App.css'
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path='checkout' element={} />
+      <Route path='checkout' element={<CheckoutPage />} />
     </Routes>
-  );
+  ); 
 }
 
 export default App
